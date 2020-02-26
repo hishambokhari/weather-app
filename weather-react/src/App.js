a26f1025860cb9ef25a5415999c35f75767b018d
root <hish.b92@gmail.com>
2020-02-26 00:20:48 +0000

    Get/Response request

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "32af43367e742b699a38fa7d379fe838",
   base: "https://home.openweathermap.org/api_keys"
 }
 
 function App() {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = evt => {
+      if (evt.key === "Enter") {
+        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result));
+      }
+    }
 
     const dateBuilder = (d) => {
       let months = ["January","February","March","April","May","June","July",
