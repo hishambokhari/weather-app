3989ee23659ad4da1b18da94e2f6f62758a58d0b
root <hish.b92@gmail.com>
2020-02-26 00:52:56 +0000

    Fix: background based on weather

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 const api = {
   key: "32af43367e742b699a38fa7d379fe838",
-  base: "https://home.openweathermap.org/api_keys"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
@@ -12,7 +12,11 @@ function App() {
       if (evt.key === "Enter") {
         fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
-        .then(result => setWeather(result));
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
       }
     }
 
@@ -31,24 +35,33 @@ function App() {
     }
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ?
+    'app warm' : 'app') :'app'}>
       <main>
         <div className="search-box">
-          <input type="text"
-          className="search-bar"
-          placeholder="Search..."
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
           <div className="temp">
-            15ºc
+            {Math.round(weather.main.temp)}°C
             </div>
-            <div className="weather">sunny</div>
+        <div className="weather">{weather.weather[0].main}</div>
+         </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
